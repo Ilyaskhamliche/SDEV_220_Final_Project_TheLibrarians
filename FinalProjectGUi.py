1dd9eb3fd381ff1d3788d43afc4a33ba88a5b737
Ilyaskhamliche <113136909+Ilyaskhamliche@users.noreply.github.com>
2023-12-13 17:06:52 -0500

    Update FinalProjectGUi.py

--- FinalProjectGUi.py
+++ FinalProjectGUi.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox 
-
+import pandas as pd
 class BookManager:
     def __init__(self, master):
         self.master = master
@@ -22,11 +22,10 @@ class BookManager:
         self.entry_PhoneNum = tk.Entry(master)
         
         # choices for the user
-        self.button_add = tk.Button(master, text="Add Book", command=self.add_book)
+        self.button_add_book = tk.Button(master, text="Add Book", command=self.add_book)
         self.button_update = tk.Button(master, text="Update Book", command=self.update_book)
         self.button_delete = tk.Button(master, text="Delete Book", command=self.delete_book)
-        self.button_add = tk.Button(master, text="Add Phone Number", command=self.add_PhoneNum)
-        # self.button_delete = tk.Button(master, text="Delete Phone Number", command=self.delete_PhoneNum)
+        self.button_add_phone = tk.Button(master, text="Add Phone Number", command=self.add_PhoneNum)
     
         # layout of the Gui
         
@@ -41,11 +40,11 @@ class BookManager:
         self.entry_publisher.grid(row=2, column=1)
         self.entry_PhoneNum.grid(row=3, column=1)
 
-        self.button_add.grid(row=3, column=0, columnspan=2, pady=10)
-        self.button_update.grid(row=4, column=0, columnspan=2, pady=10)
-        self.button_delete.grid(row=5, column=0, columnspan=2, pady=10) #number of vertical spaces
-        
-        
+        self.button_add_book.grid(row=6, column=0, columnspan=2, pady=10)        
+        self.button_update.grid(row=7, column=0, columnspan=2, pady=10)
+        self.button_delete.grid(row=8, column=0, columnspan=2, pady=10) #number of vertical spaces
+        self.button_add_phone.grid(row=9, column=0, columnspan=2, pady=10) #number of vertical spaces
+             
     def add_book(self):
         book = {
             "title": self.entry_book.get(),
@@ -93,4 +92,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = BookManager(root)
     root.mainloop()    
-
